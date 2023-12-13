ce5d47789b697272ae6beafe2e185c28e5a42303
John Adeyemo <ajohnofficial2020@gmail.com>
2023-12-13 13:11:05 -0600

    Move program code and comments with idea from the adeept rasptank

--- move.py
+++ move.py
@@ -25,6 +25,7 @@ pwm_A = None
 pwm_B = None
 
 def motorStop():
+    # Stop both motors by setting all GPIO pins to LOW
     GPIO.output(Motor_A_Pin1, GPIO.LOW)
     GPIO.output(Motor_A_Pin2, GPIO.LOW)
     GPIO.output(Motor_B_Pin1, GPIO.LOW)
@@ -44,6 +45,7 @@ def setup():
 
     motorStop()
     try:
+        # Initialize PWM objects for motor speed control
         pwm_A = GPIO.PWM(Motor_A_EN, 1000)
         pwm_B = GPIO.PWM(Motor_B_EN, 1000)
     except:
@@ -51,16 +53,19 @@ def setup():
 
 def motor_left(status, direction, speed):
     if status == 0:
+        # Stop left motor
         GPIO.output(Motor_B_Pin1, GPIO.LOW)
         GPIO.output(Motor_B_Pin2, GPIO.LOW)
         GPIO.output(Motor_B_EN, GPIO.LOW)
     else:
         if direction == Dir_backward:
+            # Set left motor direction to backward
             GPIO.output(Motor_B_Pin1, GPIO.HIGH)
             GPIO.output(Motor_B_Pin2, GPIO.LOW)
             pwm_B.start(100)
             pwm_B.ChangeDutyCycle(speed)
         elif direction == Dir_forward:
+            # Set left motor direction to forward
             GPIO.output(Motor_B_Pin1, GPIO.LOW)
             GPIO.output(Motor_B_Pin2, GPIO.HIGH)
             pwm_B.start(0)
@@ -68,16 +73,19 @@ def motor_left(status, direction, speed):
 
 def motor_right(status, direction, speed):
     if status == 0:
+        # Stop right motor
         GPIO.output(Motor_A_Pin1, GPIO.LOW)
         GPIO.output(Motor_A_Pin2, GPIO.LOW)
         GPIO.output(Motor_A_EN, GPIO.LOW)
     else:
         if direction == Dir_forward:
+            # Set right motor direction to forward
             GPIO.output(Motor_A_Pin1, GPIO.HIGH)
             GPIO.output(Motor_A_Pin2, GPIO.LOW)
             pwm_A.start(100)
             pwm_A.ChangeDutyCycle(speed)
         elif direction == Dir_backward:
+            # Set right motor direction to backward
             GPIO.output(Motor_A_Pin1, GPIO.LOW)
             GPIO.output(Motor_A_Pin2, GPIO.HIGH)
             pwm_A.start(0)
@@ -86,37 +94,47 @@ def motor_right(status, direction, speed):
 def move(speed, direction, turn, radius=0.6):
     if direction == 'forward':
         if turn == 'right':
+            # Turn right by adjusting left motor speed and direction
             motor_left(0, left_backward, int(speed * radius))
             motor_right(1, right_forward, speed)
         elif turn == 'left':
+            # Turn left by adjusting right motor speed and direction
             motor_left(1, left_forward, speed)
             motor_right(0, right_backward, int(speed * radius))
         else:
+            # Move forward with both motors at the same speed
             motor_left(1, left_forward, speed)
             motor_right(1, right_forward, speed)
     elif direction == 'backward':
         if turn == 'right':
+            # Turn right while moving backward by adjusting left motor speed and direction
             motor_left(0, left_forward, int(speed * radius))
             motor_right(1, right_backward, speed)
         elif turn == 'left':
+            # Turn left while moving backward by adjusting right motor speed and direction
             motor_left(1, left_backward, speed)
             motor_right(0, right_forward, int(speed * radius))
         else:
+            # Move backward with both motors at the same speed
             motor_left(1, left_backward, speed)
             motor_right(1, right_backward, speed)
     elif direction == 'no':
         if turn == 'right':
+            # Turn right in place by adjusting motor directions
             motor_left(1, left_backward, speed)
             motor_right(1, right_forward, speed)
         elif turn == 'left':
+            # Turn left in place by adjusting motor directions
             motor_left(1, left_forward, speed)
             motor_right(1, right_backward, speed)
         else:
+            # Stop both motors
             motorStop()
     else:
         pass
 
 def destroy():
+    # Stop both motors and clean up GPIO pins
     motorStop()
     GPIO.cleanup()
 
@@ -124,6 +142,7 @@ if __name__ == '__main__':
     try:
         speed_set = 60
         setup()
+        # Move forward for 1.3 seconds and then stop, testing motor control
         move(speed_set, 'forward', 'no', 0.8)
         time.sleep(1.3)
         motorStop()
