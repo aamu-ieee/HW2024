0b10f3500115c046d0b3b9d618c9b822241687c1
Opeyeoluwa <olanipekunopeyeoluwa1@gmail.com>
2023-12-16 22:08:27 -0600

    included comments for code blocks

--- move.py
+++ move.py
@@ -1,178 +1,154 @@
-#Library Imports
-from gpiozero import Robot, Motor, LineSensor
-import RPi.GPIO as GPIO
-import threading
+# Library Imports
 import time
+import RPi.GPIO as GPIO
 
+# Motor pins and directions
+# motor_EN_A: Pin7 | motor_EN_B: Pin11
+# motor_A: Pin8, Pin10 | motor_B: Pin13, Pin12
+Motor_A_EN = 4
+Motor_B_EN = 17
 
-"""
-    Using inbuilt libraries for robot control
-    While easy, could pose challenges down the line with
-    controlling sub units or achieveing desired results
-"""
-# Define motors
-left_motor = Motor(forward=4, backward=14)
-right_motor = Motor(forward=17, backward=27)
+Motor_A_Pin1 = 26
+Motor_A_Pin2 = 21
+Motor_B_Pin1 = 27
+Motor_B_Pin2 = 18
 
-# Create a robot with motors
-robot = Robot(left=left_motor, right=right_motor)
+Dir_forward = 0
+Dir_backward = 1
+
+left_forward = 1
+left_backward = 0
+
+right_forward = 0
+right_backward = 1
+
+pwn_A = 0
+pwm_B = 0
 
 # Define line sensors
-leftLine_sensor = LineSensor(5)
-middleLine_sensor = LineSensor(6)
-rightLine_sensor = LineSensor(7)
+leftLine_sensor = 5
+middleLine_sensor = 6
+rightLine_sensor = 7
 
-leftLine_sensor.when_line = robot.left
-rightLine_sensor.when_line = robot.right
-
-leftLine_sensor.when_no_line = robot.forward
-rightLine_sensor.when_no_line = robot.forward
-middleLine_sensor.when_line = robot.forward
-
-speed = 0.65
-
-def motor_speed():
-    while True:
-        left_detect  = int(leftLine_sensor.value)
-        right_detect = int(rightLine_sensor.value)
-        middle_detect = int(middleLine_sensor.value)
-
-        ## Stage 1
-        if left_detect == 0 and right_detect == 0:
-            left_mot = 1
-            right_mot = 1
-        ## Stage 2
-        if left_detect == 0 and right_detect == 1:
-            left_mot = -1
-        if left_detect == 1 and right_detect == 0:
-            right_mot = -1
-
-        yield (right_mot * speed, left_mot * speed)
-
-robot.source = motor_speed()
-
-time.sleep(60)
-robot.stop()
-robot.source = None
-robot.close()
-leftLine_sensor.close()
-rightLine_sensor.close()
+def motorStop():
+    # Motor stops
+    GPIO.output(Motor_A_Pin1, GPIO.LOW)
+    GPIO.output(Motor_A_Pin2, GPIO.LOW)
+    GPIO.output(Motor_B_Pin1, GPIO.LOW)
+    GPIO.output(Motor_B_Pin2, GPIO.LOW)
+    GPIO.output(Motor_A_EN, GPIO.LOW)
+    GPIO.output(Motor_B_EN, GPIO.LOW)
 
 
-"""
-    Manual method of robot control,
-    This is preferred because it gives low level control of all
-    robotic operations
-"""
-leftMotor_en = 4  
-leftMotor_pin1= 26  
-leftMotor_pin2= 21
+def setup():
+    # Motor initialization
+    global pwm_A, pwm_B
+    GPIO.setwarnings(False)
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(Motor_A_EN, GPIO.OUT)
+    GPIO.setup(Motor_B_EN, GPIO.OUT)
+    
+    GPIO.setup(Motor_A_Pin1, GPIO.OUT)
+    GPIO.setup(Motor_A_Pin2, GPIO.OUT)
+    GPIO.setup(Motor_B_Pin1, GPIO.OUT)
+    GPIO.setup(Motor_B_Pin2, GPIO.OUT)
+    
+    GPIO.setup(leftLine_sensor, GPIO.IN)
+    GPIO.setup(middleLine_sensor, GPIO.IN)
+    GPIO.setup(rightLine_sensor, GPIO.IN) 
 
-rightMotor_en = 4  
-rightMotor_pin1= 26  
-rightMotor_pin2= 21
-
-leftLine_sensor= 19  
-middleLine_sensor = 16  
-rightLine_sensor = 20
-
-def setup():  
-    GPIO.setwarnings(False)  
-    GPIO.setmode(GPIO.BCM)  
-
-    GPIO.setup(leftLine_sensor,GPIO.IN)  
-    GPIO.setup(middleLine_sensor,GPIO.IN)  
-    GPIO.setup(rightLine_sensor,GPIO.IN)  
-
-    GPIO.setup(leftMotor_en, GPIO.OUT)
-    GPIO.setup(leftMotor_pin1, GPIO.OUT)
-    GPIO.setup(leftMotor_pin2, GPIO.OUT)
-
-    GPIO.setup(rightMotor_en, GPIO.OUT)
-    GPIO.setup(rightMotor_pin1, GPIO.OUT)
-    GPIO.setup(rightMotor_pin2, GPIO.OUT)    
-
+    motorStop()
     try:
-        pwm_left = GPIO.PWM(leftMotor_en, 1000)
-        pwm_right = GPIO.PWM(rightMotor_en, 1000)
+        pwm_A = GPIO.PWM(Motor_A_EN, 1000)
+        pwm_B = GPIO.PWM(Motor_B_EN, 1000)
     except:
-        print('Unable to set Pulse Width Modulation (PWM)')
         pass
 
- 
-class Movement():
-    DIST_PER_SEC  = 35.0 # cm/s
-    SEC_PER_TURN  = 2.087 # seconds per full turn (depending on the base material, use forwardLeft(), stop() and time.time() to measure the time)
- 
-    def __init__(self, left_pin1, left_pin2, right_pin1, right_pin2):
-        self.MOTOR_LEFT_PIN1  = left_pin1
-        self.MOTOR_LEFT_PIN2  = left_pin2
-        self.MOTOR_RIGHT_PIN1 = right_pin1
-        self.MOTOR_RIGHT_PIN2 = right_pin2
- 
-        GPIO.setup(self.MOTOR_LEFT_PIN1, GPIO.OUT)
-        GPIO.setup(self.MOTOR_LEFT_PIN2, GPIO.OUT)
-        GPIO.setup(self.MOTOR_RIGHT_PIN1, GPIO.OUT)
-        GPIO.setup(self.MOTOR_RIGHT_PIN2, GPIO.OUT)
- 
-    def forwardLeft(self):
-        GPIO.output(self.MOTOR_LEFT_PIN1, GPIO.LOW)
-        GPIO.output(self.MOTOR_LEFT_PIN2, GPIO.HIGH)
-        GPIO.output(self.MOTOR_RIGHT_PIN1, GPIO.LOW)
-        GPIO.output(self.MOTOR_RIGHT_PIN2, GPIO.LOW)
-        return time.time()
- 
-    def forwardRight(self):
-        GPIO.output(self.MOTOR_RIGHT_PIN1, GPIO.LOW)
-        GPIO.output(self.MOTOR_RIGHT_PIN2, GPIO.HIGH)
-        GPIO.output(self.MOTOR_LEFT_PIN1, GPIO.LOW)
-        GPIO.output(self.MOTOR_LEFT_PIN2, GPIO.LOW)
-        return time.time()
- 
-    def forward(self):
-        GPIO.output(self.MOTOR_LEFT_PIN1, GPIO.LOW)
-        GPIO.output(self.MOTOR_LEFT_PIN2, GPIO.HIGH)
-        GPIO.output(self.MOTOR_RIGHT_PIN1, GPIO.LOW)
-        GPIO.output(self.MOTOR_RIGHT_PIN2, GPIO.HIGH)
-        return time.time()
- 
-    def backwardLeft(self):
-        GPIO.output(self.MOTOR_RIGHT_PIN1, GPIO.HIGH)
-        GPIO.output(self.MOTOR_RIGHT_PIN2, GPIO.LOW)
-        GPIO.output(self.MOTOR_LEFT_PIN1, GPIO.LOW)
-        GPIO.output(self.MOTOR_LEFT_PIN2, GPIO.LOW)
-        return time.time()
- 
-    def backwardRight(self):
-        GPIO.output(self.MOTOR_RIGHT_PIN1, GPIO.HIGH)
-        GPIO.output(self.MOTOR_RIGHT_PIN2, GPIO.LOW)
-        GPIO.output(self.MOTOR_LEFT_PIN1, GPIO.LOW)
-        GPIO.output(self.MOTOR_LEFT_PIN2, GPIO.LOW)
-        return time.time()
- 
-    def backward(self):
-        GPIO.output(self.MOTOR_LEFT_PIN1, GPIO.HIGH)
-        GPIO.output(self.MOTOR_LEFT_PIN2, GPIO.LOW)
-        GPIO.output(self.MOTOR_RIGHT_PIN1, GPIO.HIGH)
-        GPIO.output(self.MOTOR_RIGHT_PIN2, GPIO.LOW)
-        return time.time()
- 
-    def stop(self):
-        GPIO.output(self.MOTOR_LEFT_PIN1, GPIO.LOW)
-        GPIO.output(self.MOTOR_LEFT_PIN2, GPIO.LOW)
-        GPIO.output(self.MOTOR_RIGHT_PIN1, GPIO.LOW)
-        GPIO.output(self.MOTOR_RIGHT_PIN2, GPIO.LOW)
- 
-    def forwardDistance(self, dist):
-        self.forward()
-        threading.Timer(dist/self.DIST_PER_SEC, self.stop)
- 
-    def backwardDistance(self, dist):
-        self.backward()
-        threading.Timer(dist/self.DIST_PER_SEC, self.stop)
+
+def motor_left(status, direction, speed):
+    # Motor 2 positive and negative rotation
+    if status == 0:  # stop
+        GPIO.output(Motor_B_Pin1, GPIO.LOW)
+        GPIO.output(Motor_B_Pin2, GPIO.LOW)
+        GPIO.output(Motor_B_EN, GPIO.LOW)
+    else:
+        if direction == Dir_backward:
+            GPIO.output(Motor_B_Pin1, GPIO.HIGH)
+            GPIO.output(Motor_B_Pin2, GPIO.LOW)
+            pwm_B.start(100)
+            pwm_B.ChangeDutyCycle(speed)
+        elif direction == Dir_forward:
+            GPIO.output(Motor_B_Pin1, GPIO.LOW)
+            GPIO.output(Motor_B_Pin2, GPIO.HIGH)
+            pwm_B.start(0)
+            pwm_B.ChangeDutyCycle(speed)
+
+
+def motor_right(status, direction, speed):
+    # Motor 1 positive and negative rotation
+    if status == 0:  # stop
+        GPIO.output(Motor_A_Pin1, GPIO.LOW)
+        GPIO.output(Motor_A_Pin2, GPIO.LOW)
+        GPIO.output(Motor_A_EN, GPIO.LOW)
+    else:
+        if direction == Dir_forward:
+            GPIO.output(Motor_A_Pin1, GPIO.HIGH)
+            GPIO.output(Motor_A_Pin2, GPIO.LOW)
+            pwm_A.start(100)
+            pwm_A.ChangeDutyCycle(speed)
+        elif direction == Dir_backward:
+            GPIO.output(Motor_A_Pin1, GPIO.LOW)
+            GPIO.output(Motor_A_Pin2, GPIO.HIGH)
+            pwm_A.start(0)
+            pwm_A.ChangeDutyCycle(speed)
+
+
+def move(speed, direction, turn, radius=0.6):    
+    # Move the robot based on the specified speed, direction, turn, and optional radius
+    # Speed is in the range [0, 100], direction can be 'forward' or 'backward', turn can be 'right', 'left', or 'no'
+    # Radius adjusts the turning radius, where 0 < radius <= 1
+    
+    if direction == 'forward':
+        if turn == 'right':
+            motor_left(0, left_backward, int(speed*radius))
+            motor_right(1, right_forward, speed)
+        elif turn == 'left':
+            motor_left(1, left_forward, speed)
+            motor_right(0, right_backward, int(speed*radius))
+        else:
+            motor_left(1, left_forward, speed)
+            motor_right(1, right_forward, speed)
+    elif direction == 'backward':
+        if turn == 'right':
+            motor_left(0, left_forward, int(speed*radius))
+            motor_right(1, right_backward, speed)
+        elif turn == 'left':
+            motor_left(1, left_backward, speed)
+            motor_right(0, right_forward, int(speed*radius))
+        else:
+            motor_left(1, left_backward, speed)
+            motor_right(1, right_backward, speed)
+    elif direction == 'no':
+        if turn == 'right':
+            motor_left(1, left_backward, speed)
+            motor_right(1, right_forward, speed)
+        elif turn == 'left':
+            motor_left(1, left_forward, speed)
+            motor_right(1, right_backward, speed)
+        else:
+            motorStop()
+    else:
+        pass
+
+
+def destroy():
+    # Stop the motors and release GPIO resources
+    motorStop()
+    GPIO.cleanup()
 
 
 def lineDetection(leftLine_sensor, middleLine_sensor, rightLine_sensor):
+    # Detect the position of the line based on sensor readings
     status_right = GPIO.input(leftLine_sensor)  
     status_middle = GPIO.input(middleLine_sensor)  
     status_left = GPIO.input(rightLine_sensor)
@@ -185,3 +161,18 @@ def lineDetection(leftLine_sensor, middleLine_sensor, rightLine_sensor):
         print('Turn Right')
     else:
         print('Line not detected')
+		
+
+if __name__ == '__main__':
+    try:
+        speed_set = 60
+        setup()
+        move(speed_set, 'forward', 'no', 0.8)
+        time.sleep(1.3)
+        motorStop()
+        destroy()
+    except KeyboardInterrupt:
+        destroy()
+
+
+
